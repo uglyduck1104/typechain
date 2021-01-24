8027a1a69f6653c707d5c9fbf604941d46690d42
YongJun Lee <mlionlove54@gmail.com>
2021-01-24 21:39:43 +0900

    #0 TYPESCRIPT FOR BEGINNERS - First steps with Typescript

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("helllo");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Nicolas", age = "20", gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("helllo");
+const name = "Nicolas",
+    age = "20",
+    gender = "male";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
