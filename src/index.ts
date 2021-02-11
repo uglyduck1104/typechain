518e1dc357853b94fcd888855f0d9a6175ee4065
YongJun Lee <mlionlove54@gmail.com>
2021-02-11 23:50:41 +0900

    #0 TYPESCRIPT FOR BEGINNERS - Creating a Block part Three

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ class Block{
     public data: string;
     public timestamp: number;
 
-    static calculateBlcokHash = (
+    static calculateBlockHash = (
         index:number, 
         previousHash:string, 
         timestamp:number, 
@@ -35,8 +35,31 @@ let blockchain: Block[] = [genesisBlock];
 
 const getBlockchain = (): Block[] => blockchain;
 
-const getLatestBLock = (): Block => blockchain[blockchain.length - 1];
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+    const previosBlock: Block = getLatestBlock();
+    const newIndex: number = previosBlock.index + 1;
+    const newTimestamp: number = getNewTimeStamp();
+    const newHash: string = Block.calculateBlockHash(
+      newIndex,
+      previosBlock.hash,
+      newTimestamp,
+      data
+    );
+    const newBlock: Block = new Block(
+      newIndex,
+      newHash,
+      previosBlock.hash,
+      data,
+      newTimestamp
+    );
+    return newBlock;
+  };
+
+  console.log(createNewBlock("hello"));
+console.log(createNewBlock("bye bye"));
 
 export {};
